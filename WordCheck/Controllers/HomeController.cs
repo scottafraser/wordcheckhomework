c2996f2f8c8aa1734c813e5f42bbd42157db0ac2
Scott Fraser <scottafraser@yahoo.com>
2018-07-06 18:22:41 -0700

    homework

---
+++ WordCheck/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WordCheck.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+                  
+    {
+        public class HomeController : Controller
+        {
+
+            [HttpGet("/")]
+            public ActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
---
+++ WordCheck/Controllers/ItemsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WordCheck.Models;
+using System.Collections.Generic;
+
+namespace WordCheck.Controllers
+{   
+    public class ItemsController : Controller
+    {
+        [HttpGet("/categories/{categoryId}/items/new")]
+        public ActionResult CreateForm(int categoryId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category category = Category.Find(categoryId);
+            return View(category);
+        }
+        [HttpGet("/categories/{categoryId}/items/{itemId}")]
+        public ActionResult Details(int categoryId, int itemId)
+        {
+            Item item = Item.Find(itemId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category category = Category.Find(categoryId);
+            model.Add("item", item);
+            model.Add("category", category);
+            return View(item);
+        }
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
+        {
+            Item item = Item.Find(id);
+            return View(item);
+        }
+    }
+}
